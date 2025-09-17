64a7637ec080582fb1bda659c8bf6c5cefd1893a
sasa <sasa.franic@gmail.com>
2025-09-17 22:16:17 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Portum</title>
+
+</head>
+
+<body>
+    <h1>Welcome to Portum happy coding time!</h1>
+
+    <p>Hope you will enjoy your time spent on this web :)</p>
+</body>
+
+</html>
